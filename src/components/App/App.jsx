986869e4f980a76bedb6c05f775e7736395aba91
Yailene Alvarez <yailene.alvarez@gmail.com>
2024-02-29 13:00:56 -0500

    edits to sagas in store.js; created Header component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,16 @@
+import React from 'react';
+// import FavoriteView from '../FavoriteView/FavoriteView';
+// import SearchView from '../SearchView/SearchView';
+import Header from '../Header/Header';
+import './App.css';
+
+
 function App() {
   return (
-    <div>
-      <h1>Giphy Search!</h1>
+    <div className="App">
+    <Header />
+
+
     </div>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -6,12 +6,68 @@ import axios from 'axios';
 import createSagaMiddleware from 'redux-saga';
 import {takeLatest, put} from 'redux-saga/effects';
 
+const categoriesReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_CATEGORIES':
+            return action.payload;
+        default:
+            return state;
+    }
+}
 
+const favoritesReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_FAVORITES':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+const searchResults = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_SEARCHES':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+//GET from giphy api
+function* fetchGifs(action) {
+    try {
+        const gifResponse = yield axios.get('/api/giphy?q=' + action.payload);
+        yield put({type: 'SET_SEARCHES', payload: gifResponse.data});
+    } catch(err) {
+        console.log('Error getting gifs from server:', err);
+    }
+}
+
+//POST request to add to favorites
+function* postGifToFavorites(action) {
+    try {
+        yield axios.post('/api/favorites', action.payload);
+        yield put({type: 'FETCH_GIFS'});
+    } catch(err) {
+        console.log('Error adding gif to favorites:', err);
+    }
+}
+
+//PUT request to set category for fav gifs
+function* setGifCategory(action) {
+    try {
+        yield axios.put('/api/favorites/' + action.payload);
+        yield put({type: 'SET_CATEGORIES'});
+    } catch(err) {
+        console.log('Error updating fav gif category:', err);
+    }
+}
 
 // Create the rootSaga generator function
 function* rootSaga() {
-    yield takeLatest('  ', );
-    yield takeLatest('  ',);
+    yield takeLatest('FETCH_GIFS', fetchGifs);
+    yield takeLatest('ADD_TO_FAVORITES', postGifToFavorites);
+    yield takeLatest('UPDATE_CATEGORY', setGifCategory);
 }
 
 
@@ -21,7 +77,9 @@ const sagaMiddleware = createSagaMiddleware();
 // Create one store that all components can use
 const store = createStore(
     combineReducers({
-        // insert reducers here,
+        categoriesReducer,
+        favoritesReducer,
+        searchResults,
     }),
     // Add sagaMiddleware to our store
     applyMiddleware(sagaMiddleware, logger),
