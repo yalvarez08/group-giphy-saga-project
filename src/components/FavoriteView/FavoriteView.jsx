8728fa35a4b84fa909d0ad7cde2f34aedbd1a796
Beth Tracy <bntracy@gmail.com>
2024-02-29 12:38:05 -0600

    FavoritesView started

--- src/components/FavoriteView/FavoriteView.jsx
+++ src/components/FavoriteView/FavoriteView.jsx
@@ -1,11 +1,25 @@
+import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 function FavoriteView() {
     const history = useHistory();
+    const categories = useSelector(store => store.categoriesReducer);
+    const favorites = useSelector(store => store.favoritesReducer);
+
+    const displayFavorites = () => {
+        if (!favorites) {
+            return <div>You have no favorites!</div>;
+        } else {
+            return favorites.map(favorite => <li key={favorite.id}>
+                <img src={favorite.url} alt={favorite.title} />
+            </li>);
+        }
+    }
 
     return <>
         <h1>Giphy Favorites!</h1>
         <button onClick={()=>history.push('/')}>Go to Search</button>
+        {displayFavorites()}
     </>;
 }
 
