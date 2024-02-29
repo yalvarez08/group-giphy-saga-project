13381bbd34473acff2f5d965f8e646d88018ee63
keef hassan <keefhassan4@gmail.comgit config --global user.email keefhassan4@gmail.coome>
2024-02-29 11:24:06 -0600

    implement search end point

--- server/routes/giphy.router.js
+++ server/routes/giphy.router.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const pool = require('../modules/pool');
+const axios = require('axios')
 
 const router = express.Router();
 
-
+router.get('/', (req, res) => {
+    axios
+    //search endpoints and pass the search term and limit the results.
+    .get("https://api.giphy.com/v1/gifs/search?api_key="+process.env.GIPHY_API_KEY +"&q="+req.query.search+"&limit=1")
+    .then(function (response) {
+        console.log(response.data);
+        //only extracting url and title
+        const data = response.data
+        const details ={
+            url:data.data[0].url,
+            title:data.data[0].title
+        }
+        res.send(details)
+    }).catch(err => {
+        console.log(err)
+        res.sendStatus(500);
+    });
+  });
 
 
 
