7af0c8c06a551ee8918cc4229c4aaf6e0bacc193
Beth Tracy <bntracy@gmail.com>
2024-02-29 11:08:24 -0600

    SearchView started

--- src/components/SearchView/SearchView.jsx
+++ src/components/SearchView/SearchView.jsx
@@ -1,11 +1,27 @@
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
+
 function SearchView() {
+    const dispatch = useDispatch();
     const history = useHistory();
+    const [searchTerms, setSearchTerms] = useState('');
+    const searchResults = useSelector(store => store.searchresults);
+
+    const searchForGif = () => {
+        // TODO: dispatch a call to a saga with the search term as a payload
+        setSearchTerms('');
+    }
 
     return <>
         <h1>Giphy Search!</h1>
         <button onClick={()=>history.push('/favorite-view')}>Go to Favorites</button>
+        <div>
+            <input type="text" placeholder="Search" value={searchTerms} onChange={event=>setSearchTerms(event.target.value)} />
+            <button onClick={searchForGif}>Search</button>
+        </div>
+        {/* TODO: display the search results */}
     </>;
 }
 
